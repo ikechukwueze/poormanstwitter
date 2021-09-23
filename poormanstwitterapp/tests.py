986bbb17ac9d09d19de11ea6c19462acbd8ca4b5
ikechukwueze <themedric@live.com>
2021-09-23 19:01:13 +0100

    test

--- poormanstwitterapp/tests.py
+++ poormanstwitterapp/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase, SimpleTestCase
 from django.urls import reverse
 from .models import Tweet, TweetAuthor
+from .serializers import TweetSerializer
+from rest_framework import status
+import json
 
 # Create your tests here.
 
@@ -36,3 +39,36 @@ class TweetModelTest(TestCase):
         expected_tweet = f'{tweet.tweet}'
         self.assertEqual(expected_author, 'Sam')
         self.assertEqual(expected_tweet, 'Hello world.')
+
+
+class GetTweetEndPointTest(TestCase):
+    def setUp(self):
+        author = TweetAuthor.objects.create(name='Sam')
+        Tweet.objects.create(tweet_author=author, tweet='Hello world.')
+    
+    def test_get_tweet_endpoint_url(self):
+        resp = self.client.get(reverse('get_or_post_tweets'))
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_get_tweet_api(self):
+        tweets = Tweet.objects.all()
+        tweet_serializer = TweetSerializer(tweets, many=True)
+        resp = self.client.get(reverse('get_or_post_tweets'))
+        self.assertEqual(resp.data, tweet_serializer.data)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+
+class PostTweetEndPointTest(TestCase):
+    def setUp(self):
+        self.payload = {
+            'tweet_author': 'Sam',
+            'tweet': 'Hello world.'
+        }
+
+    def test_post_tweet_api(self):
+        resp = self.client.post(
+            reverse('get_or_post_tweets'),
+            data=json.dumps(self.payload),
+            content_type='application/json'
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
